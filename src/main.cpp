a38fd1c25f576ad83096731d3a5e8d8649135187
rems <rems@gmail.com>
2025-06-29 17:34:04 -0400

    feat: added test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
-#include <Arduino.h>
+/*
+ Copyright (c) 2014-present PlatformIO <contact@platformio.org>
 
-// put function declarations here:
-int myFunction(int, int);
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+**/
+
+#include <Arduino.h>
+#include <calculator.h>
+
+Calculator calc;
+
+void blink_once(unsigned int delay_ms)
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(delay_ms);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(delay_ms);
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(9600);
+    Serial.println("Program started!");
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.print("Addition: ");
+    Serial.println(calc.add(25, 17));
+    blink_once(200);
+    Serial.print("Subtraction: ");
+    Serial.println(calc.sub(10, 3));
+    blink_once(200);
+    Serial.print("Multiplication: ");
+    Serial.println(calc.mul(3, 3));
+    blink_once(200);
+    Serial.print("Division: ");
+    Serial.println(calc.div(100, 3));
+    blink_once(200);
 }
